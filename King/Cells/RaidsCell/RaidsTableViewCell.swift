46d0879a46263345c955a20637f47a45884edff2
Krushev Dmitry <krushev.d.a.@gmail.com>
2021-07-28 01:15:23 +0300

    add raid cell

---
+++ King/Cells/RaidsCell/RaidsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RaidsTableViewCell.swift
+//  King
+//
+//  Created by 1111 on 28.07.21.
+//
+
+import UIKit
+
+class RaidsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

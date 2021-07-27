29642d0cf1c72f6a3f1b63f9e91c9fca54b936a8
Krushev Dmitry <krushev.d.a.@gmail.com>
2021-07-28 00:54:08 +0300

    add cell for building tableview

---
+++ King/BuildingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BuildingTableViewCell.swift
+//  King
+//
+//  Created by 1111 on 27.07.21.
+//
+
+import UIKit
+
+class BuildingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

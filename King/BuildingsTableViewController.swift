ae70d2cd87be29c2469bbca11e3f19a9a721c8ae
Krushev Dmitry <krushev.d.a.@gmail.com>
2021-07-28 00:55:06 +0300

    add some constraints

--- King/BuildingsTableViewController.swift
+++ King/BuildingsTableViewController.swift
@@ -11,6 +11,7 @@ class BuildingsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UINib(nibName: "BuildingTableViewCell", bundle: nil), forCellReuseIdentifier: "BuildingTableViewCell")
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -23,23 +24,23 @@ class BuildingsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BuildingTableViewCell", for: indexPath) as? BuildingTableViewCell
 
         // Configure the cell...
 
-        return cell
+        return cell ?? UITableViewCell()
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.

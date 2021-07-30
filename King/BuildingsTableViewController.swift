a23d7f85e9ac2df62d9b5eb99576058adea67b15
Krushev Dmitry <krushev.d.a.@gmail.com>
2021-07-30 04:27:01 +0300

    new func for contextmenu

--- King/BuildingsTableViewController.swift
+++ King/BuildingsTableViewController.swift
@@ -2,88 +2,77 @@
 //  BuildingsTableViewController.swift
 //  King
 //
-//  Created by 1111 on 30.07.21.
+//  Created by 1111 on 27.07.21.
 //
 
 import UIKit
 
+@IBDesignable
 class BuildingsTableViewController: UITableViewController {
 
+    var buildings = ConstructedBuildings.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.register(UINib(nibName: "BuildingTableViewCell", bundle: nil), forCellReuseIdentifier: "BuildingTableViewCell")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return buildings.buildings.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BuildingTableViewCell", for: indexPath) as? BuildingTableViewCell
+        cell?.configureCell(buildings.buildings[indexPath.row].building)
         // Configure the cell...
 
-        return cell
+        return cell ?? UITableViewCell()
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let contextMenu = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ in
+            let populateBuilding = UIAction(title: "Заселить здание", image: UIImage(systemName: ""), attributes: []) { _ in
+                self.useBuilding(indexPath.row)
+            }
+            let destroyBuilding = UIAction(title: "Снести здание", image: UIImage(systemName: "trash"), attributes: [.destructive]) { _ in
+                print("Здание \(self.buildings.buildings[indexPath.row].building.buildingName) снесено")
+                self.buildings.destroy(index: indexPath.row)
+                self.tableView.reloadData()
+            }
+            return UIMenu(title: "", children: [populateBuilding,destroyBuilding])
+        }
+        
+        return contextMenu
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func useBuilding(_ index: Int){
+//        let population = Population.shared
+//        var building = buildings.buildings[index]
+//        if building.checkToUse(){
+//            self.presentAlertWithTitle(title: "Заселить здание?", message: "Вы уверены, что хотите построить это здание", options: "Нет", "Да") { (option) in
+//                switch(option) {
+//                    case 1:
+//                        building.use()
+//                        self.tableView.reloadData()
+//                        break
+//                    default:
+//                        break
+//                }
+//            }
+//        } else {
+//            self.presentAlertWithTitle(title: "Недостаточно людей для заселения постройки", message: "", options: "Ок"){_ in }
+//        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- King/NewBuildingsTableViewController.swift
+++ King/NewBuildingsTableViewController.swift
@@ -33,51 +33,34 @@ class NewBuildingsTableViewController: UITableViewController {
         cell.configureCell(buildings[indexPath.row])
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let contextMenu = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ in
+            let addNewBuilding = UIAction(title: "Построить здание", image: UIImage(systemName: "plus"), attributes: []) { _ in
+                guard let cell = tableView.cellForRow(at: indexPath) as? BuildingTableViewCell else {return}
+                guard let building = cell.building else {return}
+                self.addBuilding(building)
+            }
+            return UIMenu(title: "", children: [addNewBuilding])
+        }
+        return contextMenu
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func addBuilding(_ building: Building){
+        if Resources.shared.gold>=building.buildingCost.gold, Resources.shared.scince>=building.buildingCost.scince{
+            self.presentAlertWithTitle(title: "Построить здание?", message: "Вы уверены, что хотите построить это здание", options: "Нет", "Да") { (option) in
+                switch(option) {
+                    case 1:
+                        Resources.shared.gold -= building.buildingCost.gold
+                        ConstructedBuildings.shared.buildings += [ConstructedBuilding(building)]
+                        print("построено новое здание \(building.buildingName)")
+                        break
+                    default:
+                        break
+                }
+            }
+        } else {
+            self.presentAlertWithTitle(title: "Недостаточно ресурсов для постройки", message: "", options: "Ок"){_ in }
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

facbe70b2031262272430568b8ea11735bb4c917
Krushev Dmitry <krushev.d.a.@gmail.com>
2021-07-30 04:26:19 +0300

    add alert extention

---
+++ King/UIViewController+Alert.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Alert.swift
+//  King
+//
+//  Created by 1111 on 30.07.21.
+//
+
+import UIKit
+
+extension UIViewController {
+
+    func presentAlertWithTitle(title: String, message: String, options: String..., completion: @escaping (Int) -> Void) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        for (index, option) in options.enumerated() {
+            alertController.addAction(UIAlertAction.init(title: option, style: .default, handler: { (action) in
+                completion(index)
+            }))
+        }
+        self.present(alertController, animated: true, completion: nil)
+    }
+}

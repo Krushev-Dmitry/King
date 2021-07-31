2aed27c3f8e683fd4ff9550fd37ba6bc454e080f
Krushev Dmitry <krushev.d.a.@gmail.com>
2021-07-31 14:05:19 +0300

    change alert style

--- King/UIViewController+Alert.swift
+++ King/UIViewController+Alert.swift
@@ -10,12 +10,13 @@ import UIKit
 extension UIViewController {
 
     func presentAlertWithTitle(title: String, message: String, options: String..., completion: @escaping (Int) -> Void) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
         for (index, option) in options.enumerated() {
             alertController.addAction(UIAlertAction.init(title: option, style: .default, handler: { (action) in
                 completion(index)
             }))
         }
         self.present(alertController, animated: true, completion: nil)
+
     }
 }

1f6251b8d9ab007f18348c97581f2d8a5fc178bf
Krushev Dmitry <krushev.d.a.@gmail.com>
2021-07-31 15:41:57 +0300

    change detail view

---
+++ King/View/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  King
+//
+//  Created by 1111 on 31.07.21.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
